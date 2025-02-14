405a1636128d2b9f8bba9eb954df7362d3070ffd
walianitin <walianitin828@gmail.com>
2025-02-14 23:16:48 +0530

    auth

--- live-song/app/lib/auth-options.ts
+++ live-song/app/lib/auth-options.ts
@@ -1,5 +1,8 @@
 
 import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+import prisma from "./db";
+import { NextResponse } from "next/server";
 
 
 export const authOptions={
@@ -8,5 +11,24 @@ export const authOptions={
           clientId: process.env.GOOGLE_CLIENT_ID??" ",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET??""
         })
-      ]
+      ],callback:{
+        async signIn(params:any){
+          if(!params.user.email) {return false}
+          try {
+            await prisma.user.create({
+              data:{
+                email:params.user.email,
+                provider:"Google"
+              }
+            })
+          }catch(e:any){
+            return NextResponse.json({
+              message:"error in the lib auth option callback",
+              error:e.message,
+            })
+          }
+          return true;
+
+        }
+      }
 }
