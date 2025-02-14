08aa06fa5e9480d8d16c40a40daa245a6ccaa8a8
walianitin <walianitin828@gmail.com>
2025-02-15 00:09:49 +0530

    streamapi

--- live-song/app/api/stream/route.ts
+++ live-song/app/api/stream/route.ts
@@ -1,21 +1,20 @@
 import { authOptions } from "@/app/lib/auth-options";
 import { getServerSession } from "next-auth";
-import { getSession } from "next-auth/react";
+
 import { NextRequest, NextResponse } from "next/server";
-import { format } from "path";
-import {z} from "zod"
+
+import { z} from "zod"
 //@ts-ignore
-import {yt_rgx} from "youtube-regex"
-import { constants } from "buffer";
-import { db } from "@/app/db";
-const CreatoStreamSchema=   z.object({
+
+import  db  from "@/app/lib/db";
+const createStreamSchema=z.object({
 
     creatorId:z.string(),
     url:z.string(),
    spaceId:z.string()
 })
 export async function POST(req:NextRequest,res:NextResponse){
-
+    console.log(await res.json());
     const session=await  getServerSession(authOptions);
     try{
         if(!session?.user){
@@ -28,17 +27,24 @@ export async function POST(req:NextRequest,res:NextResponse){
     //here is the main logic
 //        const session=await getServerSession(authOptions);
         const user=session?.user;
-        const data= CreatoStreamSchema.parse(await req.json());
-
-        const isYT=yt_rgx().test(data.url);
-        if(!isYT) {
+        const data= createStreamSchema.parse(await req.json());
+        console.log("stream is trigerred")
+            try{
+                const isYT=yt_rgx().test(data.url);
+                if(!isYT) {
+                    return NextResponse.json({
+                        message:"the yt link is not valid",
+                        status:403,
+                    })
+                }
+        }catch(e){
             return NextResponse.json({
-                message:"the yt link is not valid",
-                status:403,
+                message:"error in the yt regex"
             })
         }
+                
         const videoId = data.url ? data.url.match(yt_rgx)?.[1] : null;
-        if (!isYT || !videoId) {
+        if (!videoId) {
           return NextResponse.json(
             {
               message: "Invalid YouTube URL format",
@@ -51,16 +57,18 @@ export async function POST(req:NextRequest,res:NextResponse){
 
         const stream = await db.stream.create({
             data: {
-            creatorId: data.creatorId,
+            id: data.creatorId,
             url: data.url,
             spaceId: data.spaceId,
-            videoId: videoId,
+            //@ts-ignore
+            videoId:videoId
             },
         });
 
         return NextResponse.json({
             data:{
-                message:"hello the streams is working "
+                message:"stream created ",
+                streamId:stream.id
             }
         })
 } catch (error) {
