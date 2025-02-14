b120b54a59ecba38f418afcb9a505fb0726faac3
walianitin <walianitin828@gmail.com>
2025-02-14 23:30:13 +0530

    issue resolved

--- live-song/app/lib/auth-options.ts
+++ live-song/app/lib/auth-options.ts
@@ -5,30 +5,32 @@ import prisma from "./db";
 import { NextResponse } from "next/server";
 
 
-export const authOptions={
- providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID??" ",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET??""
-        })
-      ],callback:{
-        async signIn(params:any){
-          if(!params.user.email) {return false}
-          try {
-            await prisma.user.create({
-              data:{
-                email:params.user.email,
-                provider:"Google"
-              }
-            })
-          }catch(e:any){
-            return NextResponse.json({
-              message:"error in the lib auth option callback",
-              error:e.message,
-            })
-          }
-          return true;
-
-        }
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn(params: any) {
+      console.log("SignIn callback triggered", params);
+      if (!params.user.email) {
+        return false;
       }
-}
+      try {
+        await prisma.user.create({
+          data: {
+            email: params.user.email,
+            provider: "Google",
+          },
+        });
+        console.log("User created successfully");
+      } catch (e: any) {
+        console.error("Error creating user:", e.message);
+        throw new Error("Failed to create user");
+      }
+      return true;
+    },
+  },
+};
